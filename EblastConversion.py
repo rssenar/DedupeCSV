3f679633a0e8f2c8f97283f292ccf22e9a426d33
Richard Senar <rssenar@gmail.com>
2016-04-05 20:03:53 -0700

    fixed bugs EblastConv program

--- EblastConversion.py
+++ EblastConversion.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.4
 # ---------------------------------------------
 from __future__ import division, print_function
 import csv, os, glob, requests
@@ -30,47 +30,61 @@ def PURLConversion():
 			FirstLine = False
 		else:
 			if FirstTime is True:
-				PURL = 'http://' + line[0]
+				PURL = '{}{}'.format(
+					'http://',
+					line[0]
+					)
 				F_PURL = requests.get(PURL)
-				F_PURL = str(F_PURL.url) + '8'
-				FName = line[1] + line[2]
+				F_PURL = '{}{}'.format(str(F_PURL.url),'8')
+				FName = '{}{}'.format(
+					line[1],
+					line[2]
+					)
 				URL = F_PURL.split(FName)
 				HeaderURL = URL[0]
 				FooterURL = URL[1]
-				New_PURL = HeaderURL + FName + FooterURL
-				Output.writerow((\
-					New_PURL,\
-					line[1],\
-					line[2],\
-					line[3],\
-					line[4],\
-					line[5],\
-					line[6]\
+				New_PURL = '{}{}{}'.format(
+					HeaderURL,
+					FName,
+					FooterURL
+					)
+				Output.writerow((
+					New_PURL,
+					line[1],
+					line[2],
+					line[3],
+					line[4],
+					line[5],
+					line[6]
 					))
 				FirstTime = False
 			else:
-				FName = line[1] + line[2]
-				New_PURL = HeaderURL + FName + FooterURL
-				Output.writerow((\
-					New_PURL,\
-					line[1],\
-					line[2],\
-					line[3],\
-					line[4],\
-					line[5],\
-					line[6]\
+				FName = '{}{}'.format(
+					line[1],
+					line[2]
+					)
+				New_PURL = '{}{}{}'.format(
+					HeaderURL,
+					FName,
+					FooterURL
+					)
+				Output.writerow((
+					New_PURL,
+					line[1],
+					line[2],
+					line[3],
+					line[4],
+					line[5],
+					line[6]
 					))
 # ---------------------------------------------
 for index in range(0,len(CSVFiles)):
 	FirstTime = True
 	FirstLine = True
-	InputFile = open(CSVFiles[index],'rU')
-	Input = csv.reader(InputFile)
-	OutputFile = open('_EBLAST_' + str(CSVFiles[index]),'ab')
-	Output = csv.writer(OutputFile)
-	Output.writerow(HeaderRow)
-	PURLConversion()
-# ---------------------------------------------
-OutputFile.close()
-InputFile.close()
-# ---------------------------------------------
+	with open(CSVFiles[index],'rU') as InputFile, \
+	open('EBLAST_' + str(CSVFiles[index]),'at') as OutputFile:
+		Input = csv.reader(InputFile)
+		Output = csv.writer(OutputFile)
+		next(InputFile)
+		Output.writerow(HeaderRow)
+		PURLConversion()
