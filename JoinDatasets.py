a6df8fa9dd834670a96166b02d49e83ef33efe11
Richard Senar <rssenar@gmail.com>
2016-04-07 14:47:09 -0700

    refactored JoinDataset

--- JoinDatasets.py
+++ JoinDatasets.py
@@ -7,18 +7,19 @@ from tqdm import tqdm
 # ---------------------------------------------
 os.chdir('../../../../Desktop/')
 # ---------------------------------------------
-Seq = 1
-ziproute = 2
-Description = 3
-Records = 4
-total = 5
-dfo = 6
-Percentage = 7
-RTotal = 8
+Seq = 0
+ziproute = 1
+Description = 2
+Records = 3
+total = 4
+dfo = 5
+Percentage = 6
+RTotal = 7
+AdjRec = 8
+RecRTotal = 9
 
 OutputHeaderRow = [
-	'Seq1',
-	'Seq2',
+	'Seq',
 	'ziproute',
 	'Description',
 	'Records',
@@ -26,6 +27,8 @@ OutputHeaderRow = [
 	'dfo',
 	'Percentage',
 	'RTotal',
+	'AdjRec',
+	'RecRTotal'
 	]
 
 def perc(part, whole):
@@ -35,37 +38,38 @@ def perc(part, whole):
 		return 100 * float(part)/float(whole)
 
 def Join():
-	#File1 = input('Enter 1st Dataset: ')
-	#FileName1 = '{}.csv'.format(File1)
-	#File2 = input('Enter 2nd Dataset: ')
-	#FileName2 = '{}.csv'.format(File2)
-	#ds1 = pd.read_csv(FileName1)
-	#ds2 = pd.read_csv(FileName2)
-	# ---------------------------------------------	
 	ds1 = pd.read_csv('a.csv')
 	ds2 = pd.read_csv('b.csv')
 	# ---------------------------------------------
 	merged = ds1.merge(ds2, how='inner')
-	merged.to_csv('temp1.csv', encoding='utf-8')
-	df = pd.read_csv('temp1.csv')
-	df['Percentage'] = ''
-	df['RTotal'] = ''
-	df.to_csv('temp2.csv')
-
+	merged['Percentage'] = ''
+	merged['RTotal'] = '' 
+	merged['AdjRec'] = '' 
+	merged['AdjRecRTotal'] = '' 
+	merged.to_csv('temp.csv', encoding='utf-8')
+	
 def ReformatOutputReport():
-	CSVFiles = glob.glob('temp2.csv')
+	CSVFiles = glob.glob('temp.csv')
 	for File in tqdm(CSVFiles):
-		with open(File,'rU') as InputFile, open('REFINED DATA_.csv','at') as OutputFile:
+		with open(File,'rU') as InputFile,\
+		open('REFINED DATA_.csv','at') as OutputFile:
 			Input = csv.reader(InputFile)
 			Output = csv.writer(OutputFile)
 			Output.writerow(OutputHeaderRow)
 			RunningTotal = 0
+			AdjRecRTotal = 0
 			next(InputFile)
 			for row in tqdm(Input):
 				if int(row[Records]) >= 135: 
 					row[Percentage] = round(perc(row[Records],row[total]),0)
 					RunningTotal += int(row[Records])
 					row[RTotal] = RunningTotal
+					if int(row[Percentage]) >= 74:
+						row[AdjRec] = round(float(row[total]) * 0.73,0)
+					else:
+						row[AdjRec] = row[Records]
+					AdjRecRTotal += int(row[AdjRec])
+					row[RecRTotal] = AdjRecRTotal
 					Output.writerow(row)
 
 if __name__ == '__main__':
@@ -73,7 +77,6 @@ if __name__ == '__main__':
 	ReformatOutputReport()
 	Files = glob.glob('*.csv')
 	for Record in Files:
-		if bool(re.match(r'\btemp1\b', Record, flags = re.I)):
+		if bool(re.match(r'\btemp\b', Record, flags = re.I)):
 			os.remove(Record)
-		if bool(re.match(r'\btemp2\b', Record, flags = re.I)):
-			os.remove(Record)
+
