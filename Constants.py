1969d4d5e3672328e6e850b33e9f5231cb0bb560
Richard Senar <rssenar@gmail.com>
2016-04-10 20:34:54 -0700

    modified Constants.py file

---
+++ Constants.py
@@ -0,0 +1,92 @@
+# Constant Variables
+
+CustomerID = 0
+FullName = 1
+FirstName = 2
+MI = 3
+LastName = 4
+Address1 = 5
+Address2 = 6
+AddressComb = 7
+City = 8
+State = 9
+Zip = 10
+Zip4 = 11
+SCF = 12
+Phone = 13
+HPhone = 14
+WPhone = 15
+MPhone = 16
+Email = 17
+VIN = 18
+Year = 19
+Make = 20
+Model = 21
+DelDate = 22
+Date = 23
+Radius = 24
+Coordinates = 25
+VINLen = 26
+DSF_WALK_SEQ = 27
+CRRT = 28
+ZipCRRT = 29
+KBB = 30
+BuybackValues = 31
+WinningNum = 32
+MailDNQ = 33
+BlitzDNQ = 34
+Drop = 35
+PURL = 36
+YrDec = 37
+SCF3DFacility = 38
+Vendor = 39
+Misc1 = 40
+Misc2 = 41
+Misc3 = 42
+
+# Header Output list
+HeaderRowMain = [
+	'CustomerID',
+	'FullName',
+	'FirstName',
+	'MI',
+	'LastName',
+	'Address1',
+	'Address2',
+	'AddressFull',
+	'City',
+	'State',
+	'Zip',
+	'4Zip',
+	'SCF',
+	'Phone',
+	'HPH',
+	'BPH',
+	'CPH',
+	'Email',
+	'VIN',
+	'Year',
+	'Make',
+	'Model',
+	'DelDate',
+	'Date',
+	'Radius',
+	'Coordinates',
+	'VINLen',
+	'DSF_WALK_SEQ',
+	'Crrt',
+	'ZipCrrt',
+	'KBB',
+	'BuybackValue',
+	'WinningNumber',
+	'MailDNQ',
+	'BlitzDNQ',
+	'Drop',
+	'PURL',
+	'YrDec',
+	'SCF3DFacility',
+	'Vendor',
+	'Misc1',
+	'Misc2',
+	'Misc3'
+	]
