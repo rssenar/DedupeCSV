df987ecb047a4ee774fd39ae0cc4ca5785fccc9c
Richard Senar <rssenar@gmail.com>
2016-04-10 20:48:38 -0700

    Extracted Constant variables and save into a stand alone Contants.py file

--- NormalizeFile.py
+++ NormalizeFile.py
@@ -3,6 +3,7 @@
 # ---------------------------------------------------------------------------- #
 import csv, os, sys, re, glob, collections, datetime, subprocess
 from dateutil.parser import *
+from Constants import *
 from geopy.distance import vincenty
 from nameparser import HumanName
 from tqdm import tqdm
@@ -60,9 +61,9 @@ for file in CSVFile:
 try:
 	DropDict = {}
 	with open(DropFile,'rU') as DropFile:
-		Drop = csv.reader(DropFile)
+		DropRec = csv.reader(DropFile)
 		next(DropFile)
-		for line in Drop:
+		for line in DropRec:
 			DropDict[line[0]] = line[1]
 except:
 	print('..... ERROR: Unable to Load Drop Dictionary File')
@@ -337,97 +338,6 @@ CleanOutputPurchaseAll = '{}_UPLOAD.csv'.format(IPFName)
 CleanOutputAppendP = '{}_PENNY.csv'.format(IPFName)
 CleanOutputAppendN = '{}_NICKEL.csv'.format(IPFName)
 CleanOutputAppendR = '{}_OTHER.csv'.format(IPFName)
-# ---------------------------------------------------------------------------- #
-CustomerID = 0
-FullName = 1
-FirstName = 2
-MI = 3
-LastName = 4
-Address1 = 5
-Address2 = 6
-AddressComb = 7
-City = 8
-State = 9
-Zip = 10
-Zip4 = 11
-SCF = 12
-Phone = 13
-HPhone = 14
-WPhone = 15
-MPhone = 16
-Email = 17
-VIN = 18
-Year = 19
-Make = 20
-Model = 21
-DelDate = 22
-Date = 23
-Radius = 24
-Coordinates = 25
-VINLen = 26
-DSF_WALK_SEQ = 27
-CRRT = 28
-ZipCRRT = 29
-KBB = 30
-BuybackValues = 31
-WinningNum = 32
-MailDNQ = 33
-BlitzDNQ = 34
-Drop = 35
-PURL = 36
-YrDec = 37
-SCF3DFacility = 38
-Vendor = 39
-Misc1 = 40
-Misc2 = 41
-Misc3 = 42
-
-# Header Output list
-HeaderRowMain = [
-	'CustomerID',
-	'FullName',
-	'FirstName',
-	'MI',
-	'LastName',
-	'Address1',
-	'Address2',
-	'AddressFull',
-	'City',
-	'State',
-	'Zip',
-	'4Zip',
-	'SCF',
-	'Phone',
-	'HPH',
-	'BPH',
-	'CPH',
-	'Email',
-	'VIN',
-	'Year',
-	'Make',
-	'Model',
-	'DelDate',
-	'Date',
-	'Radius',
-	'Coordinates',
-	'VINLen',
-	'DSF_WALK_SEQ',
-	'Crrt',
-	'ZipCrrt',
-	'KBB',
-	'BuybackValue',
-	'WinningNumber',
-	'MailDNQ',
-	'BlitzDNQ',
-	'Drop',
-	'PURL',
-	'YrDec',
-	'SCF3DFacility',
-	'Vendor',
-	'Misc1',
-	'Misc2',
-	'Misc3'
-	]
 # ---------------------------------------------------------------------------- #
 # Compare File Header row to HeaderRowMain to determin if re-mapping required
 ExtractCSVHeader = subprocess.check_output(['head','-n','1',InputFile])
