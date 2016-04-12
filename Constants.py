f4ceee0e8b74abf76da93c7babf8c75781ddc71a
Richard Senar <rssenar@gmail.com>
2016-04-11 18:58:54 -0700

    Added DoNotMailSet, YearDecodeDict & USStatesDict

--- Constants.py
+++ Constants.py
@@ -168,3 +168,50 @@ def ConvPercentage(part, whole):
 	else:
 		return 100 * float(part)/float(whole)
 # ---------------------------------------------------------------------------- #
+DoNotMailSet = set([
+	'inc', 'inc.', 'incorporated', 'international', 'corporation', 'corp', 
+	'corp.', 'construction', 'const', 'const.', 'prof', 'prof.', 'professional', 
+	'service', 'services', 'consultancy', 'consultant', 'consultants', 'living', 
+	'trust', 'llc', 'enterprise', 'enterprises', 'infrastructure', 'the', 
+	'resource', 'resources', 'cooperative', 'cooperatives', 'comp', 'company', 
+	'companies', 'store', 'dealer', 'dealers', 'dealership', 'dealerships', 
+	'fleet', 'office', 'station', 'health', 'acura', 'am general', 'audi', 
+	'bmw', 'buick', 'cadillac', 'chevrolet', 'chrysler', 'daewoo', 'dodge', 
+	'ferrari', 'fiat', 'gmc', 'honda', 'hummer', 'hyundai', 'infiniti', 'isuzu', 
+	'jaguar', 'jeep', 'kia', 'land rover', 'lexus', 'lincoln', 'maserati', 
+	'maybach', 'mazda', 'mercedes', 'mercedes-benz', 'mercury', 'mini', 
+	'mitsubishi', 'nissan', 'oldsmobile', 'plymouth', 'pontiac', 'porsche',
+	'ram', 'saab', 'saturn', 'scion', 'smart', 'subaru', 'suzuki', 'toyota',
+	'volkswagen', 'volvo', 'auto', 'automotive', 'group' 
+	])
+
+YearDecodeDict = dict([
+	(0,2000), (1,2001), (2,2002), (3,2003), (4,2004), (5,2005), (6,2006), 
+	(7,2007), (8,2008), (9,2009), (10,2010), (11,2011), (12,2012), (13,2013), 
+	(14,2014), (15,2015), (16,2016), (17,2017), (18,2018), (19,2019), (20,2020), 
+	(40,1940), (41,1941), (42,1942), (43,1943), (44,1944), (45,1945), (46,1946), 
+	(47,1947), (48,1948), (49,1949), (50,1950), (51,1951), (52,1952), (53,1953), 
+	(54,1954), (55,1955), (56,1956), (57,1957), (58,1958), (59,1959), (60,1960), 
+	(61,1961), (62,1962), (63,1963), (64,1964), (65,1965), (66,1966), (67,1967), 
+	(68,1968), (69,1969), (70,1970), (71,1971), (72,1972), (73,1973), (74,1974), 
+	(75,1975), (76,1976), (77,1977), (78,1978), (79,1979), (80,1980), (81,1981), 
+	(82,1982), (83,1983), (84,1984), (85,1985), (86,1986), (87,1987), (88,1988), 
+	(89,1989), (90,1990), (91,1991), (92,1992), (93,1993), (94,1994), (95,1995), 
+	(96,1996), (97,1997), (98,1998), (99,1999)
+	])
+
+USStatesDict = {
+	'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa',
+	'AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut',
+	'DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia',
+	'GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois',
+	'IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts',
+	'MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri',
+	'MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National',
+	'NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire',
+	'NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio',
+	'OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico',
+	'RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee',
+	'TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont',
+	'WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'
+	}
