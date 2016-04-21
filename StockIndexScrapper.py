e9c5eb90a1ccb0ea950256aafb7981df35b0e4af
Richard Senar <rssenar@gmail.com>
2016-04-21 15:04:57 -0700

    updated syntax on index scrapper

--- StockIndexScrapper.py
+++ StockIndexScrapper.py
@@ -13,43 +13,51 @@ url = 'http://finance.yahoo.com/;_ylt=AwrTcdCNdhhXy7MAk3cnnIlQ;_ylu=X3oDMTByb2lv
 response = requests.get(url)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 # ------------------------- #
+# US Index
 SNP500 = soup.find(id="yfs_l84_^gspc").get_text()
+SNP500 = CleanUpReturn(SNP500)[0]
+
 Dow = soup.find(id="yfs_l84_^dji").get_text()
-Nasdaq = soup.find(id="yfs_l84_^ixic").get_text()
-
-Nikkei = soup.find(id="yfs_l84_^n225").get_text()
-HangSeng = soup.find(id="yfs_l84_^hsi").get_text()
-SSE = soup.find(id="yfs_l84_000001.ss").get_text()
-
-FTSE = soup.find(id="yfs_l84_^ftse").get_text()
-DAX = soup.find(id="yfs_l84_^gdaxi").get_text()
-CAC40 = soup.find(id="yfs_l84_^fchi").get_text()
-# ------------------------- #
-SNP500 =  CleanUpReturn(SNP500)[0]
 Dow = CleanUpReturn(Dow)[0]
+
+Nasdaq = soup.find(id="yfs_l84_^ixic").get_text()
 Nasdaq = CleanUpReturn(Nasdaq)[0]
+# ------------------------- #
+# Asia Index
+Nikkei = soup.find(id="yfs_l84_^n225").get_text()
+Nikkei = CleanUpReturn(Nikkei)[0]
 
-Nikkei =  CleanUpReturn(Nikkei)[0]
+HangSeng = soup.find(id="yfs_l84_^hsi").get_text()
 HangSeng = CleanUpReturn(HangSeng)[0]
-SSE = CleanUpReturn(SSE)[0]
 
-FTSE =  CleanUpReturn(FTSE)[0]
+SSE = soup.find(id="yfs_l84_000001.ss").get_text()
+SSE = CleanUpReturn(SSE)[0]
+# ------------------------- #
+# Europe Index
+FTSE = soup.find(id="yfs_l84_^ftse").get_text()
+FTSE = CleanUpReturn(FTSE)[0]
+
+DAX = soup.find(id="yfs_l84_^gdaxi").get_text()
 DAX = CleanUpReturn(DAX)[0]
+
+CAC40 = soup.find(id="yfs_l84_^fchi").get_text()
 CAC40 = CleanUpReturn(CAC40)[0]
 # ------------------------- #
-print()
+print('--------------')
 print('US')
 print('--------------')
 print('{} | S&P 500  - ${}'.format(datetime.now(), SNP500))
 print('{} | Dow      - ${}'.format(datetime.now(), Dow))
 print('{} | Nasdaq   - ${}'.format(datetime.now(), Nasdaq))
 print()
+print('--------------')
 print('ASIA')
 print('--------------')
 print('{} | Nikkeo   - {}'.format(datetime.now(), Nikkei))
 print('{} | HangSeng - {}'.format(datetime.now(), HangSeng))
 print('{} | SSE      - {}'.format(datetime.now(), SSE))
 print()
+print('--------------')
 print('EUROPE')
 print('--------------')
 print('{} | FTSE     - {}'.format(datetime.now(), FTSE))
@@ -57,6 +65,4 @@ print('{} | DAX      - {}'.format(datetime.now(), DAX))
 print('{} | CAC40    - {}'.format(datetime.now(), CAC40))
 print()
 print('All Done!')
-print()
 # ------------------------- #
-
